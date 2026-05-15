59bd11e693b0552f7d2d564c78ce96bd7d41a498
tedikuswandi85-gif <tedikuswandi85@admin.smp.belajar.id>
2026-05-15 21:22:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,32 @@ const db = firebase.database();
 
 // === 2. DATABASE UJIAN (KONFIGURASI MULTI-GURU) ===
 const databaseUjian = [
-    // Contoh IPS: Kelas 7A & 7B Link sama (Guru A), Kelas 7C Link beda (Guru B)
-    { tingkat: ["7A", "7B"], mapel: "IPS", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-02", durasi: 90, token: "IPSJAYA" },
-    { tingkat: ["7C"], mapel: "IPS", link: "https://forms.gle/LinkIPS_GuruB", tgl: "2026-06-02", durasi: 90, token: "IPSHEBAT" },
+   // Contoh IPS: Kelas 7A & 7B Link sama (Guru A), Kelas 7C Link beda (Guru B)
+    { tingkat: ["7A", "7B", "7C"], mapel: "PAI-BP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-02", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Bahasa Indonesia", link: "https://forms.gle/XqWryHkcxdyRSEC99", tgl: "2026-06-02", durasi: 90, token: "AYEKAPTEN!" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Pendidikan Pancasila", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Bahasa Inggris", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "SBDP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-04", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Matematika", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-04", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "IPA", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-05", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Informatika", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-08", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "IPS", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-08", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "Bahasa Sunda", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-09", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["7A", "7B", "7C"], mapel: "PJOK", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-09", durasi: 90, token: "IPSJAYA" },
+
+    { tingkat: ["8A", "8B", "8C"], mapel: "PAI-BP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-02", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 90, token: "AYEKAPTEN!" },
+    { tingkat: ["8C"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 90, token: "AYEKAPTEN!" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Pendidikan Pancasila", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Bahasa Inggris", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "SBDP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-04", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Matematika", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-04", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "IPA", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-05", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Informatika", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-08", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "IPS", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-08", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Bahasa Sunda", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-09", durasi: 90, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "PJOK", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-09", durasi: 90, token: "IPSJAYA" },
+
     
     // Mapel lain yang linknya sama untuk satu angkatan
     { tingkat: ["9A", "9B"], mapel: "PABP", link: "https://bit.ly/PSAJPAIBP2526", tgl: "2026-05-07", durasi: 90, token: "PABP9" },
