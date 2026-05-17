0557987bdb3f5c837efbbafbbfb7b8cfa7bdae4b
tedikuswandi85-gif <tedikuswandi85@admin.smp.belajar.id>
2026-05-17 22:16:51 +0700

    Restore Firebase configuration and exam database

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ const databaseUjian = [
     { tingkat: ["7A", "7B", "7C"], mapel: "Bahasa Sunda", link: "https://forms.gle/NKfxpkok1yYKQd1NA", tgl: "2026-06-09", durasi: 90, token: "SUNDAGAMPANG26" },
     { tingkat: ["7A", "7B", "7C"], mapel: "PJOK", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-09", durasi: 90, token: "IPSJAYA" },
 
+    // Contoh IPS: Kelas 88 & 8B Link sama (Guru A), Kelas 8C Link beda (Guru B)
     { tingkat: ["8A", "8B", "8C"], mapel: "PAI-BP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-02", durasi: 90, token: "IPSJAYA" },
     { tingkat: ["8A", "8B"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 120, token: "AYEKAPTEN!" },
     { tingkat: ["8C"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 120, token: "AYEKAPTEN!" },
