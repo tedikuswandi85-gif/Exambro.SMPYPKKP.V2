c695685451dff8f96c558f23eafbe35018a76f52
tedikuswandi85-gif <tedikuswandi85@admin.smp.belajar.id>
2026-05-19 08:36:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const databaseUjian = [
     { tingkat: ["8A", "8B"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 120, token: "AYEKAPTEN!" },
     { tingkat: ["8C"], mapel: "Bahasa Indonesia", link: "https://forms.gle/YaPELxt9tMnsJM7v7", tgl: "2026-06-02", durasi: 120, token: "AYEKAPTEN!" },
     { tingkat: ["8A", "8B", "8C"], mapel: "Pendidikan Pancasila", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 90, token: "IPSJAYA" },
-    { tingkat: ["8A", "8B", "8C"], mapel: "Bahasa Inggris", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-03", durasi: 120, token: "IPSJAYA" },
+    { tingkat: ["8A", "8B", "8C"], mapel: "Bahasa Inggris", link: "https://forms.gle/JTHZsYMbuz2Kk9Ne9", tgl: "2026-06-03", durasi: 120, token: "SAVEOURWORLD!" },
     { tingkat: ["8A", "8B", "8C"], mapel: "SBDP", link: "https://forms.gle/LinkIPS_GuruA", tgl: "2026-06-04", durasi: 90, token: "IPSJAYA" },
     { tingkat: ["8A", "8B", "8C"], mapel: "Matematika", link: "https://docs.google.com/forms/d/e/1FAIpQLSfcgWckXWfByn1YXDQ26daB4H1fUuk2Yd8HIfS7uOF63ADSyg/viewform?usp=publish-editor", tgl: "2026-06-04", durasi: 120, token: "PSAT2026" },
     { tingkat: ["8B", "8C"], mapel: "IPA", link: "https://forms.gle/uTBkYMzcexhttwHH9", tgl: "2026-06-05", durasi: 120, token: "PSATIPA8BC26" },
